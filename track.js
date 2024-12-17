642de1e18d1996c314df2c718a6ff08161b32e77
schenna217 <90796839+schenna217@users.noreply.github.com>
2024-12-17 00:13:50 -0600

    Create ball functions and file

--- track.js
+++ track.js
@@ -6,6 +6,7 @@ class Track {
 
     draw(ctx) {
         ctx.beginPath();
+        // x-coordinate, y-coordintate, radius, 0 for beginning angle, pi * 2 for the end angle to form full circle
         ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
         ctx.strokeStyle = "white";
         ctx.stroke();
