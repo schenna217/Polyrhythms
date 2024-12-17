f123a5ba691a287baf9efd508fe4c922f29c7d33
schenna217 <90796839+schenna217@users.noreply.github.com>
2024-12-17 00:22:01 -0600

    Created constructor and draw function for ball js

---
+++ ball.js
@@ -0,0 +1,17 @@
+class Ball {
+    constructor(track, radius, speed) {
+        this.track = track;
+        this.radius = radius;
+        this.speed = speed;
+        // Control ball at beginning of track
+        this.offset = 0;
+        this.center = this.track.getPosition(this.offset);
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+    }
+}
