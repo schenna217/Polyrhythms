7a820cbf3ad2500ba59c6399ae8e385bb100f497
schenna217 <90796839+schenna217@users.noreply.github.com>
2024-12-16 23:58:42 -0600

    Added draw function

--- track.js
+++ track.js
@@ -4,5 +4,10 @@ class Track {
         this.radius = radius;
     }
 
-    
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+    }
 }
