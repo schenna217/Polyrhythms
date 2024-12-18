f172c7b75c270914c1a73361cb4fb59775c57bde
schenna217 <90796839+schenna217@users.noreply.github.com>
2024-12-17 23:56:42 -0600

    More updates

--- track.js
+++ track.js
@@ -4,6 +4,14 @@ class Track {
         this.radius = radius;
     }
 
+
+    getPosition(offset) {
+        return {
+            x: this.center.x + Math.cos(offset) * this.radius,
+            y: this.center.y - Math.sin(offset) * this.radius
+        }
+    }
+
     draw(ctx) {
         ctx.beginPath();
         // x-coordinate, y-coordintate, radius, 0 for beginning angle, pi * 2 for the end angle to form full circle
