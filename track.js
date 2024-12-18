1c394474ee8329dc42891d67d5efe9443390c2f8
schenna217 <90796839+schenna217@users.noreply.github.com>
2024-12-18 00:34:02 -0600

    Modified circle function
    Understood math behind code, how to modify shapes and will work on efficiency of code.

--- track.js
+++ track.js
@@ -7,7 +7,7 @@ class Track {
 
     getPosition(offset) {
         return {
-            x: this.center.x + Math.cos(offset) * this.radius,
+            x: this.center.x + Math.cos(offset * 3) * this.radius,
             y: this.center.y - Math.sin(offset) * this.radius
         }
     }
@@ -15,7 +15,17 @@ class Track {
     draw(ctx) {
         ctx.beginPath();
         // x-coordinate, y-coordintate, radius, 0 for beginning angle, pi * 2 for the end angle to form full circle
-        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+            // ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        
+        // New method by creating large amount of dots along path of circle, creates jagged circle
+        for (let a = 0; a < Math.PI * 2; a += 0.1) {
+            ctx.lineTo(
+                this.center.x + Math.cos(a * 3) * this.radius,
+                this.center.y - Math.sin(a) * this.radius
+            );
+        }
+        ctx.closePath();
+
         ctx.strokeStyle = "white";
         ctx.stroke();
     }
